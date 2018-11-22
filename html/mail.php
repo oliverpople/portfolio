ad303c788c2312d8f26427c95ab9786486a02912
oliverpople <oliverpople@gmail.com>
2018-11-22 14:55:48 -0800

    Second back up

--- html/mail.php
+++ html/mail.php
@@ -19,7 +19,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "example@example.com";
+        $recipient = "oliverpople@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
@@ -51,4 +51,3 @@
     }
 
 ?>
-
